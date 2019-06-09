b63ddbeff8051ff9664e7dbd64a6231a59a306db
eduardosenshi <43457388+eduardosenshi@users.noreply.github.com>
2019-06-09 20:14:27 -0300

    Add files via upload
    .

--- index.php
+++ index.php
@@ -1,148 +1,28 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Dashboard</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="assets/css/main.css">
-        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-        <link rel="stylesheet" href="assets/css/bootstrap-grid.css">
-        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    </head>
-    <body>
-        <!--[if lt IE 7]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
-        
-        <?php 
+<!DOCTYPE>
+<html lang="pt-br">
+ <head>
+ 	<title>PROMAPA ASSESSORIA</title>
+ 	<link href="https://fonts.googleapis.com/css?family=Dosis|Nanum+Gothic|Raleway&display=swap" rel="stylesheet">
+ 	<link rel="icon" type="imagem/png" href="icon.png" />
+  <meta charset="UTF-8"/>
+     <link rel="stylesheet" type="text/css" href="style.css"/>
+ </head>
+      <frameset cols="20%,80%,*" border="0px">
+        <frame src="menu.html" resize="no"></frame>
+        <frame src="corpo.html" Name="site" noresize="yes"></frame>
+      </frameset>
+      <link rel="stylesheet" type="text/css" href="style.css"/>
+    <?php 
 
             session_start();
     
             if(!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] != 'SIM'){
-                header('Location: login.php?login=erro2');
+                header('Location: php/login.php?login=erro2');
             }
         ?>
+	<body>
 
-        <section class="navbar-container">
-            <div class="container-fluid px-5">
-                <div class="row no-gutters">
-                    <div class="logo-wrapper">
-                        <h4>Dashboard</h4>
-                    </div>
-                    <div class="user-section ml-auto">
-                        <div class="user-name">
-                            <h6>Bem-vindo, <span><?php echo $_SESSION['userlogado'];?>! | </span><a class="" href="logoff.php">Sair</a></h6>     
-                        </div>
-                         
-                    </div>
-                </div>
-            </div>
-        </section>   
+		
+	</body>
 
-        <section class="sidebar col-3">
-            <div class="menu-wrapper">
-                <ul>
-                    <li><a href="#">Dashboard</a></li>
-                    <li><a href="#">Todos Protocolos</a></li>
-                    <li><a href="#">Novo Protocolo</a></li>
-                    <li><a href="#">Usuários</a></li>
-                </ul>
-            </div>
-        </section>
-
-        <section class="main-content col-9 ml-auto">
-            <div class="container">
-                <div class="row no-gutters">
-                    <div class="main-content-title">
-                        <h3>Dashboard</h3>
-                    </div>
-                </div>
-
-                <div class="card-container">
-                    <div class="row no-gutters">
-
-                        <div class="col-3">
-                            <div class="card">
-                                <div class="icon-wrapper">
-                                    <i class="fa fa-users"></i>
-                                </div>
-
-                                <div class="text-container">
-                                    <h4>Todos os Protocolos</h4>
-                                    <h6>26</h6>
-                                </div>
-                                <div class="card-desc">
-                                    <h5>Ver todos os protocolos</h5>
-                                    <i class="fa fa-arrow"></i>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-3">
-                            <div class="card">
-                                <div class="icon-wrapper">
-                                    <i class="fa fa-users"></i>
-                                </div>
-
-                                <div class="text-container">
-                                    <h4>Todos os Usuários</h4>
-                                    <h6>6</h6>
-                                </div>
-                                <div class="card-desc">
-                                    <h5>Ver todos os usuários</h5>
-                                    <i class="fa fa-arrow"></i>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-3">
-                            <div class="card">
-                                <div class="icon-wrapper">
-                                    <i class="fa fa-users"></i>
-                                </div>
-
-                                <div class="text-container">
-                                    <h4>Usuários Banidos</h4>
-                                    <h6>14</h6>
-                                </div>
-                                <div class="card-desc">
-                                    <h5>Ver todos os usuários</h5>
-                                    <i class="fa fa-arrow"></i>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-3">
-                            <div class="card">
-                                <div class="icon-wrapper">
-                                    <i class="fa fa-users"></i>
-                                </div>
-
-                                <div class="text-container">
-                                    <h4>Usuários Pendentes</h4>
-                                    <h6>26</h6>
-                                </div>
-                                <div class="card-desc">
-                                    <h5>Ver todos os usuários pendentes</h5>
-                                    <i class="fa fa-arrow"></i>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            
-            </div>
-        </section>
-
-
-        <script src="assets/js/jquery.js" async defer></script>
-        <script src="assets/js/bootstrap.min.js" async defer></script>
-        <script src="assets/js/main.js" async defer></script>
-    </body>
 </html>
