e63930f8a1e81035dae81431908532762270ae08
Snewor <43658904+Snewor@users.noreply.github.com>
2019-06-10 21:37:46 -0300

    Correção no login

--- assets/js/main.js
+++ assets/js/main.js
@@ -171,12 +171,12 @@ window.Promapa = {
             Promapa.Ajax.deslogarUsuario();
         });
     },
-	Validate: 
+	Validate: {
 		validateEmail: function(email){
 			 var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 			return re.test(String(email).toLowerCase());
 		},
-		
+        	
 		validateAll: function(){
 			var message = '';
 			var valid = false;
@@ -184,53 +184,52 @@ window.Promapa = {
 			if($("#cnpj").val().trim() == '' || $("#cnpj").val() == null || $("#cnpj").val() < 18){
 				message = 'Insira um CNPJ válido';
 				valid = false;
-				break;
+				
 			};
 			if($("#razaoSocial").val().trim() == '' || $("#razaoSocial").val() == null || $("#razaoSocial").val() < 5){
 				message = 'Insira uma razão social válida!';
 				valid = false;
-				break;
+				
 			};
 			if($("#nomeFantasia").val().trim() == '' || $("#nomeFantasia").val() == null || $("#nomeFantasia").val() < 5){
 				message = 'Insira um nome fantasia válido válida!';
 				valid = false;
-				break;
+				
 			};
 			if($("#dataConstituicao").val().trim() == '' || $("#dataConstituicao").val() == null){
 				message = 'Insira uma data de constituição válida!';
 				valid = false;
-				break;
+				
 			};
 			
 			var dataConstituicao =  $("#dataConstituicao").val().split('/');
-			var date = new date(dataConstituicao[2] + "," + dataConstituicao[1] +  "," + dataConstituicao[0]);
+			var date = new Date(dataConstituicao[2] + "," + dataConstituicao[1] +  "," + dataConstituicao[0]);
 			
 			if(date > new Date()){
 				message = 'A data de constituição não pode ser maior que a data atual!';
 				valid = false;
-				break;
+				
 			};
 			
 			if($("#telefone").val().trim() == '' || $("#telefone").val() == null || $("#telefone").val() < 13){
 				message = 'Insira um telefone válido!';
 				valid = false;
-				break;
+				
 			};
 			
-			if($("#email").val().trim() == '' || $("#email").val() == null || $("#email").val() < 13 || !Promapa.Validate.testEmail($("#email").val())){
+			if($("#email").val().trim() == '' || $("#email").val() == null || $("#email").val() < 13 || !Promapa.Validate.validateEmail($("#email").val())){
 				message = 'Insira um email válido!';
 				valid = false;
-				break;
+				
 			};
 			
 			valid = true;
 			
 			return valid;
+        }
 			
-		},
-	
-		
-	},
+    }
+
 }
 
 $(document).ready(function(){
